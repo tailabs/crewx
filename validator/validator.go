7453902dc2347d7653f4157506d6cc2df8b1eedb
iutx <root@viper.run>
2024-01-13 15:34:35 +0800

    optimize validator test cases
    Signed-off-by: iutx <root@viper.run>

--- validator/validator.go
+++ validator/validator.go
@@ -8,13 +8,20 @@ import (
 	"github.com/mitchellh/mapstructure"
 )
 
+// MapStruct decodes a map into a struct using mapstructure and validates the struct using the validator package.
+// It returns an error if decoding or validation fails.
 func MapStruct(m any, i any) error {
+	// Decode the map to the struct using mapstructure
 	if err := mapstructure.Decode(m, &i); err != nil {
 		return fmt.Errorf("failed to decode map to struct: %w", err)
 	}
 
+	// Create a new validator instance with support for required fields
 	v := validator.New(validator.WithRequiredStructEnabled())
+
+	// Validate the struct using the validator
 	if err := v.Struct(i); err != nil {
+		// Handle validation errors
 		var errs validator.ValidationErrors
 		if errors.As(err, &errs) {
 			errFields := make([]string, 0, len(errs))
--- validator/validator_test.go
+++ validator/validator_test.go
@@ -1,21 +1,65 @@
 package validator
 
-import "testing"
+import (
+	"testing"
 
-func Test_MapStruct(t *testing.T) {
-	type Struct struct {
-		AccessToken string `mapstructure:"access_token" validate:"required"`
-		Secret      string `validate:"required"`
+	"github.com/stretchr/testify/require"
+)
+
+type TestStruct struct {
+	Name  string `validate:"required,min=3,max=20"`
+	Email string `validate:"required,email"`
+}
+
+func TestMapStruct(t *testing.T) {
+	tests := []struct {
+		name        string
+		inputMap    map[string]interface{}
+		expectedErr bool
+	}{
+		{
+			name: "Valid Input",
+			inputMap: map[string]interface{}{
+				"Name":  "John Doe",
+				"Email": "john@example.com",
+			},
+			expectedErr: false,
+		},
+		{
+			name: "Invalid Input - Missing Required Field",
+			inputMap: map[string]interface{}{
+				"Name": "John Doe",
+				// Missing "Email" field
+			},
+			expectedErr: true,
+		},
+		{
+			name: "Invalid Input - Invalid Email",
+			inputMap: map[string]interface{}{
+				"Name":  "John Doe",
+				"Email": "invalid-email",
+			},
+			expectedErr: true,
+		},
+		{
+			name: "Invalid Input - Decode Failure",
+			inputMap: map[string]interface{}{
+				"InvalidField": "value",
+			},
+			expectedErr: true,
+		},
 	}
 
-	mapData := map[string]string{
-		"access_token": "this_is_access_token",
-		"secret":       "this_is_secret",
-	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			var example TestStruct
+			err := MapStruct(tc.inputMap, &example)
 
-	s := Struct{}
-
-	if err := MapStruct(mapData, s); err != nil {
-		t.Fatal(err)
+			if tc.expectedErr {
+				require.Error(t, err, "Expected an error but got nil")
+			} else {
+				require.NoError(t, err, "Expected no error but got %v", err)
+			}
+		})
 	}
 }
